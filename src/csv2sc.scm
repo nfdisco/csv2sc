48cc87c1cb6daa4e7b5367047e4d9753704f6291
nfdisco <nr9@posteo.de>
2019-05-12 16:00:45 +0200

    Separate CSV parser from SC writer.

--- src/csv2sc.scm
+++ src/csv2sc.scm
@@ -24,34 +24,6 @@
   "Raise a csv-error."
   (throw 'csv-error msg))
 
-(define (csv-split-record str field-sep quote-char ignore-ws)
-  "Split a CSV record into fields."
-  (define (iter lst quoted field record)
-    (cond
-     ((null? lst)
-      (cond
-       (quoted
-        (csv-error "mismatched quotation character"))
-       (else
-        (reverse (cons (reverse-list->string field) record)))))
-     (else
-      (let ((char (car lst)))
-        (cond
-         ((and quote-char
-               (char=? char quote-char))
-          (iter (cdr lst) (not quoted) field record))
-         ((and ignore-ws
-               (not quoted)
-               (char-whitespace? char))
-          (iter (cdr lst) quoted field record))
-         ((and (not quoted)
-               (char=? char field-sep))
-          (iter (cdr lst) #f (list)
-                (cons (reverse-list->string field) record)))
-         (else
-          (iter (cdr lst) quoted (cons char field) record)))))))
-  (iter (string->list str) #f (list) (list)))
-
 (define (csv-field->number str dec-sep-char)
   "Parse a numeric CSV field.  Return #f if field is non-numeric."
   (cond
@@ -68,6 +40,39 @@
                           (string-replace str "." i (1+ i) 0 1)
                           str))))))
 
+(define (csv-parse-record str field-sep dec-sep-char quote-char ignore-ws)
+  "Parse CSV record, return list of fields."
+  (define (iter lst quoted current fields)
+    (cond
+     ((null? lst)
+      (cond
+       (quoted
+        (csv-error "mismatched quotation character"))
+       (else
+        (reverse (cons (reverse-list->string current) fields)))))
+     (else
+      (let ((char (car lst)))
+        (cond
+         ((and quote-char
+               (char=? char quote-char))
+          (iter (cdr lst) (not quoted) current fields))
+         ((and ignore-ws
+               (not quoted)
+               (char-whitespace? char))
+          (iter (cdr lst) quoted current fields))
+         ((and (not quoted)
+               (char=? char field-sep))
+          (iter (cdr lst)
+                #f
+                (list)
+                (cons (reverse-list->string current) fields)))
+         (else
+          (iter (cdr lst) quoted (cons char current) fields)))))))
+  (map (lambda (field)
+         (or (csv-field->number field dec-sep-char)
+             field))
+       (iter (string->list str) #f (list) (list))))
+
 (define (sc-cell-coords row-num col-num)
   "Return the coordinates of a cell in alphanumeric notation, e.g. \"Z42\"."
   (define (column-name)
@@ -82,10 +87,9 @@
     (list->string (iter (1- col-num) (list))))
   (string-append (column-name) (number->string row-num)))
 
-(define (sc-put-cell str row-num col-num dec-sep-char right-align)
+(define (sc-put-cell value row-num col-num right-align)
   "Write a cell to the standard output in SC format."
-  (let ((place (sc-cell-coords row-num col-num))
-        (value (or (csv-field->number str dec-sep-char) str)))
+  (let ((place (sc-cell-coords row-num col-num)))
     (cond
      ((number? value)
       (format #t "let ~A = ~A\n" place value))
@@ -96,25 +100,29 @@
               place
               value)))))
 
+(define (sc-put-record values row-num right-align)
+  "Write record to the standard output in SC format."
+  (define (iter values col-num)
+    (when (not (null? values))
+          (sc-put-cell (car values) row-num col-num right-align)
+          (iter (cdr values) (1+ col-num))))
+  (iter values 1))
+
 (define (csv->sc field-sep-char quote-char ignore-ws dec-sep-char right-align)
   "Read CSV-formatted data from the standard input and write it in SC format
 to the standard output."
   (define (iter row-num)
-    (define (put-cells str)
-      (define (iter lst col-num)
-        (when (not (null? lst))
-              (sc-put-cell (car lst) col-num row-num dec-sep-char right-align)
-              (iter (cdr lst) (1+ col-num))))
-      (iter (csv-split-record str field-sep-char quote-char ignore-ws) 1))
     (let ((line (get-line (current-input-port))))
-      (when (not (eof-object? line))
-            (catch 'csv-error
-                   (lambda ()
-                     (put-cells line))
-                   (lambda (key msg)
-                     (warning "skipping line no. ~A: ~A\n" row-num msg)))
-            (iter (1+ row-num)))))
-    (iter 1))
+      (when
+       (not (eof-object? line))
+       (catch 'csv-error
+              (lambda ()
+                (sc-put-record (csv-parse-record line field-sep-char
+                  dec-sep-char quote-char ignore-ws) row-num right-align))
+              (lambda (key msg)
+                (warning "skipping line no. ~A: ~A\n" row-num msg)))
+       (iter (1+ row-num)))))
+  (iter 1))
 
 (define (apply-command-line-arguments args)
   "Apply command-line arguments to csv->sc."
